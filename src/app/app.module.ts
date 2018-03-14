4b74f048c14d88ab13fe8e5e390127f35cdef4e1
vbarsuchenko <vladimir.barsuchenko@gmail.com>
2018-03-14 23:32:49 +0200

    Added SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'angular-tuned' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
