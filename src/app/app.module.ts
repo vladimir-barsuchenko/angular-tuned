dc05753f3f2fdcae58841cab2d6e2024fe13e7d9
vbarsuchenko <vladimir.barsuchenko@gmail.com>
2018-03-15 21:56:57 +0200

    Router. Lazy load everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'angular-tuned' }),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DemoComponent } from './demo.component';
+import { DemoRoutingModule } from './demo-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+  ],
+  declarations: [DemoComponent],
+})
+export class DemoModule {}
