01428d3aaad4dfd4f144d3aa5498da31cb2aec37
vbarsuchenko <vladimir.barsuchenko@gmail.com>
2018-03-25 18:57:06 +0300

    fix pr marked as fixme:

--- src/app/modules/service-worker-config.module.ts
+++ src/app/modules/service-worker-config.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 // use local sw fix until it's merged
-// https://github.com/angular/angular/pull/21231
+// FIXME: https://github.com/angular/angular/pull/21231
 import { ServiceWorkerModule, SwPush } from './src';
 
 
