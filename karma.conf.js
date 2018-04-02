71bee8588c69126802b67ce026fa3407cf23daa4
vbarsuchenko <vladimir.barsuchenko@gmail.com>
2018-04-02 18:49:56 +0300

    fix(travis): Use headless chrome
    Use headless chrome

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+		browsers: ['HeadlessChrome'],
+		customLaunchers: {
+			HeadlessChrome: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			}
+		},
     singleRun: false
   });
 };
