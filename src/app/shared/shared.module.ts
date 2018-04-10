6d69b130f12caab3856289ac783b36100e23a933
vbarsuchenko <vladimir.barsuchenko@gmail.com>
2018-04-10 23:43:27 +0300

    feat: added flex-layout deps
    added flex-layout deps

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
   ],
   declarations: [],
 })
