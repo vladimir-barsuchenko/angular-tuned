050c86dde4fed59cd36e7a0875f15b514e4c0a2c
levgaas <vladimir.barsuchenko@gmail.com>
2018-07-28 11:54:18 +0300

    test(sandbox): fixed test

--- src/app/features/sandbox/sandbox.component.spec.ts
+++ src/app/features/sandbox/sandbox.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SandboxComponent } from './sandbox.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SandboxComponent', () => {
   let component: SandboxComponent;
@@ -8,6 +9,7 @@ describe('SandboxComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [SandboxComponent],
     })
     .compileComponents();
